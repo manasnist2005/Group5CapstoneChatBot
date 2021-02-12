22dec14045a2c49676dec2ba55c6bffa5fbc1adc
manasnist2005 <manasnist2005@gmail.com>
2021-02-13 01:42:29 +0530

    Update industry-bot.js

--- industry-bot.js
+++ industry-bot.js
@@ -1,23 +1,25 @@
 var loadingMsgIndex,
     botui = new BotUI('industry-bot'),
-    country = '1',
-    local = '1',
-	gender = '1',
-	date = '2012-04-23',
-	isection = '1',
-	eType = '1',
-	risk = '0',
+    country = '',
+    local = '',
+	gender = '',
+	date = '',
+	isection = '',
+	eType = '',
+	risk = '',
     API = 'https://nlp-bot-batch5.herokuapp.com/predict',
-    coutryLookup = [{text: 'Country_01',value: '1'}, {text: 'Country_02',value: '2'},{text: 'Country_03',  value: '3'}],
-	localLookup1 = [{text: 'Local_01',value: '1'}, {text: 'Local_03',value: '2'}, {text: 'Local_04',value: '4'}, {text: 'Local_06',value: '6'}, {text: 'Local_11',value: '11'}],
-	localLookup2 = [{text: 'Local_02',value: '2'}, {text: 'Local_05',value: '5'}, {text: 'Local_07',value: '7'}, {text: 'Local_08',value: '8'}, {text: 'Local_09',value: '9'}, {text: 'Local_12',value: '12'}],
-	localLookup3 = [{text: 'Local_10',value: '10'}],
-	industryLookup = [{text: 'Mining',value: '1'},{text: 'Metals',value: '2'},{text: 'others',value: '3'}],
-	genderLookup = [{text: 'Male',value: '1'},{text: 'Female',value: '2'}],
-	eTypeLookup = [{text: 'Employee',value: '1'},{text: 'Third Party',value: '2'},{text: 'Third Party (Remote)',value: '3'}],
-	riskLookup = [{text: 'Not Applicable',value: '0'},{text: 'Bees',value: '1'},{text: 'Blocking and isolation of energies)',value: '2'}];
+    coutryLookup = [{text: 'Country_01',value: 'Country_01'}, {text: 'Country_02',value: 'Country_02'},{text: 'Country_03',  value: 'Country_03'}],
+	localLookup1 = [{text: 'Local_01',value: 'Local_01'}, {text: 'Local_03',value: 'Local_03'}, {text: 'Local_04',value: 'Local_04'}, {text: 'Local_06',value: 'Local_06'}, {text: 'Local_11',value: 'Local_11'}],
+	localLookup2 = [{text: 'Local_02',value: 'Local_02'}, {text: 'Local_05',value: 'Local_05'}, {text: 'Local_07',value: 'Local_07'}, {text: 'Local_08',value: 'Local_08'}, {text: 'Local_09',value: 'Local_09'}, {text: 'Local_12',value: 'Local_12'}],
+	localLookup3 = [{text: 'Local_10',value: 'Local_10'}],
+	industryLookup = [{text: 'Mining',value: 'Mining'},{text: 'Metals',value: 'Metals'},{text: 'others',value: 'others'}],
+	genderLookup = [{text: 'Male',value: 'Male'},{text: 'Female',value: 'Female'}],
+	eTypeLookup = [{text: 'Employee',value: 'Employee'},{text: 'Third Party',value: 'Third Party'},{text: 'Third Party (Remote)',value: 'Third Party (Remote)'}],
 	
-
+	riskLookup = [{text: 'Not applicable',value: 'Not applicable'},{text: 'Pressed',value: 'Pressed'},{text: 'Pressurized Systems',value: 'Pressurized Systems'},{text: 'Manual Tools',value: 'Manual Tools'},{text: 'Others',value: 'Others'},{text: 'Fall prevention (same level)',value: 'Fall prevention (same level)'},{text: 'Chemical substances',value: 'Chemical substances'},{text: 'Liquid Metal',value: 'Liquid Metal'},{text: 'Electrical installation',value: 'Electrical installation'},{text: 'Confined space',value: 'Confined space'},{text: 'Pressurized Systems / Chemical Substances',value: 'Pressurized Systems / Chemical Substances'},{text: 'Blocking and isolation of energies',value: 'Blocking and isolation of energies'},{text: 'Suspended Loads',value: 'Suspended Loads'},{text: 'Poll',value: 'Poll'},{text: 'Cut',value: 'Cut'},{text: 'Fall',value: 'Fall'},{text: 'Bees',value: 'Bees'},{text: 'Fall prevention',value: 'Fall prevention'},{text: 'Traffic',value: 'Traffic'},{text: 'Projection',value: 'Projection'},{text: 'Venomous Animals',value: 'Venomous Animals'},{text: 'Plates',value: 'Plates'},{text: 'Projection/Burning',value: 'Projection/Burning'},{text: 'remains of choco',value: 'remains of choco'},{text: 'Vehicles and Mobile Equipment',value: 'Vehicles and Mobile Equipment'},{text: 'Projection/Choco',value: 'Projection/Choco'},{text: 'Machine Protection',value: 'Machine Protection'},{text: 'Power lock',value: 'Power lock'},{text: 'Burn',value: 'Burn'},{text: 'Projection/Manual Tools',value: 'Projection/Manual Tools'},{text: 'Individual protection equipment',value: 'Individual protection equipment'},{text: 'Electrical Shock',value: 'Electrical Shock'},{text: 'Projection of fragments',value: 'Projection of fragments'}];
+	
+var askCountry = function () {
+botui.message.removeAll();
 botui.message
   .bot('Select your country :')
   .then(function () {
@@ -37,10 +39,11 @@ botui.message
 	askLocal();
   
 });
+}
 
 var askLocal = function () {
 	
-if(country == '1'){
+if(country == 'Country_01'){
 	botui.message
   .bot('Select your Local :')
   .then(function () {
@@ -60,7 +63,7 @@ if(country == '1'){
 	askIndustrySection();
 });
 	}
-else if(country == '2'){
+else if(country == 'Country_02'){
 	botui.message
   .bot('Select your Local :')
   .then(function () {
@@ -80,7 +83,6 @@ else if(country == '2'){
 	askIndustrySection();
 });
 }
-
 else{
 	botui.message
   .bot('Select your Local :')
@@ -181,7 +183,7 @@ var askCriticalRisk = function () {
     return botui.action.select({
 	action: {
 	placeholder : "Select Risk",
-	value: '1', // Selected value or selected object. Example: {value: "TR", text : "Türkçe" }
+	value: 'Pressed', // Selected value or selected object. Example: {value: "TR", text : "Türkçe" }
 	searchselect : true, // Default: true, false for standart dropdown
 	label : 'text', // dropdown label variable
 	options : riskLookup,
@@ -206,36 +208,33 @@ var askDate = function () {
     return botui.action.text({
       delay: 1000,
       action: {
-        value: '2012-04-23',
-        placeholder: 'YYYY-MM-DD'
+        value: '2016-01-01',
+        placeholder: '2016-01-01'
       }
     })
 }).then(function (res) {   
-    botui.message.human({
-      delay: 500,
-      content: res.value
-    });
+    
 	date = res.value;
 	console.log("Date :"+ date); 
-	init();
+	askKey();
 });
 
 }
 
 
 
-function init() {
+function askKey() {
   botui.message
   .bot({
     delay: 1000,
-    content: 'Enter the keyword to find the accident level:'
+    content: 'Provide the description to find the Accident and Potential Accident level:'
   })
   .then(function () {
     return botui.action.text({
       delay: 1000,
       action: {
-        value: 'pulley',
-        placeholder: 'pulley'
+        value: '',
+        placeholder: ''
       }
     })
   }).then(function (res) {
@@ -249,10 +248,43 @@ function init() {
   });
 }
 
+var continuefunc = function(){
+	botui.message
+  .bot('Do you want to Continue ?')
+  .then(function () {
+    return botui.action.button({
+      delay: 1000,
+      addMessage: false, // so we could the address in message instead if 'Existing Address'
+      action: [{
+        text: 'YES',
+        value: 'yes'
+      }, {
+        text: 'NO',
+        value: 'no'
+      }]
+    })
+}).then(function (res) {
+	botui.message.human({
+      delay: 500,
+      content: res.text
+    });
+  if(res.value == 'yes') {       
+    askKey();
+  } else {
+     botui.message
+    .bot({
+      delay: 500,
+      content: 'Thank you for chatting with us !!'
+    });
+  }
+});
+}
+
+
 
 function sendXHR1(key,showAccidentLevel) {
     
-var text = "{\"accident_deatils\": {\"Country\": "+country+",\"Local\": "+local+",\"Industry_Sector\": "+isection+",\"Gender\": "+gender+",\"Employee_Type\": "+eType+",\"Critical_Risk\": "+risk+",\"Date\": \""+date+"\",\"Description\":\""+key+"\"}}"
+var text = "{\"accident_deatils\": {\"Country\": \""+country+"\",\"Local\": \""+local+"\",\"Industry_Sector\": \""+isection+"\",\"Gender\": \""+gender+"\",\"Employee_Type\": \""+eType+"\",\"Critical_Risk\": \""+risk+"\",\"Date\": \""+date+"\",\"Description\":\""+key+"\"}}"
 
   
   $.ajax({
@@ -261,73 +293,52 @@ var text = "{\"accident_deatils\": {\"Country\": "+country+",\"Local\": "+local+
             dataType: 'json',
             contentType: 'application/json',
 			data: text,
-            success: function (data) {
-                showAccidentLevel(data['pedict']);
-            }            
+            success: function (data) {                
+				showAccidentLevel(data['Accident pedict'],data['Potential Accident pedict'])
+            },
+			error: function (err) {                
+				showErrorMessage(err)
+            }
+			
         });
 }
 
 
-function showAccidentLevel(lvl) {
+function showAccidentLevel(lvl1,lvl2) {
   botui.message
   .update(loadingMsgIndex, {
-    content: 'Accident Level :' + (lvl || "Not found")  
+    content: 'Accident Level :' + (lvl1 || "Not found") +' Potential Accident Level :' + (lvl2 || "Not found")
   })
-  .then(init); // ask again for repo. Keep in loop.
+  .then(continuefunc); // ask again for repo. Keep in loop.
 }
 
-
-/*
-function sendXHR1(key) {
-  var alvl = LoadJson(key);
-  showAccidentLevel(alvl);
-}
-
-
-function LoadJson(key){
-	var accident_deatils = {
-          "Country": country,
-		  "Local": local,
-		  "Industry_Sector":isection,
-		  "Gender":gender,
-		  "Employee_Type":eType,
-		  "Critical_Risk":risk,
-		  "Date" :date,
-		  "Description":key
-       };
-	//var url = "url?data=" + encodeURIComponent(JSON.stringify({"country": country, "local": local, "gender": gender, "isection": isection, "eType": eType,"":}));
-	//console.log(url);
-	var json = null;
-    $.ajax({
-        'async': false,
-        'global': false,
-        'url': API,
-		'body' : accident_deatils,
-        'dataType': "json",
-        'success': function (data) {
-            json = data;
-        }
-		
-    });
-return(json[key]);
-console.log(json[key]);
-}
-
-function sendXHR(repo, cb) {
-  var xhr = new XMLHttpRequest();
-  var self = this;
-  xhr.open('GET', API + repo);
-  xhr.onload = function () {
-    var res = JSON.parse(xhr.responseText)
-    cb(res.stargazers_count);
-  }
-  xhr.send();
-}
-function showStars(stars) {
+function showErrorMessage(err) {
   botui.message
   .update(loadingMsgIndex, {
-    content: 'it has !(star) ' + (stars || "0") + ' stars.'
+    content: 'Something wrong with the request'
   })
-  .then(init); // ask again for repo. Keep in loop.
+  .then(continuefunc); // ask again for repo. Keep in loop.
 }
-*/
+
+$( ".cancel" ).on( "click", function() {	
+  document.getElementById("myForm").style.display = "none";
+    country = '';
+    local = '';
+	gender = '';
+	date = '';
+	isection = '';
+	eType = '';
+	risk = '';
+	botui.message.removeAll();
+});
+$( ".open-button" ).on( "click", function() {
+    country = '';
+    local = '';
+	gender = '';
+	date = '';
+	isection = '';
+	eType = '';
+	risk = '';	
+  askCountry();
+});
+
